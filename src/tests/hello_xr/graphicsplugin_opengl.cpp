fa75b5381ff4959aac55072703bd6ef24902aed0
danwillm <39023874+danwillm@users.noreply.github.com>
2024-12-31 14:30:19 +0000

    tests/hello_xr: Implement double buffering for vulkan

--- src/tests/hello_xr/graphicsplugin_opengl.cpp
+++ src/tests/hello_xr/graphicsplugin_opengl.cpp
@@ -299,7 +299,7 @@ struct OpenGLGraphicsPlugin : public IGraphicsPlugin {
     }
 
     void RenderView(const XrCompositionLayerProjectionView& layerView, const XrSwapchainImageBaseHeader* swapchainImage,
-                    int64_t swapchainFormat, const std::vector<Cube>& cubes) override {
+                    int64_t swapchainFormat, uint32_t /* viewIndex */, const std::vector<Cube>& cubes) override {
         CHECK(layerView.subImage.imageArrayIndex == 0);  // Texture arrays not supported.
         UNUSED_PARM(swapchainFormat);                    // Not used in this function for now.
 
